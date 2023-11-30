fc2444b317a8b3bc3580ecc95a6f164576b6b88d
ken6078 <ken60786213@gmail.com>
2023-11-30 10:38:42 +0800

    feat: activity apply * must confirm to sending apply * fix image wrong bug

--- NIU App/NIU App/Activity/Apply/ActivityApplyViewController.swift
+++ NIU App/NIU App/Activity/Apply/ActivityApplyViewController.swift
@@ -12,15 +12,9 @@ class ActivityApplyViewController: UIViewController {
     let screenSize:CGRect = UIScreen.main.bounds
     
     var activity: Activity = Activity()
+    var activityApplyViewModel: ActivityApplyViewModel = ActivityApplyViewModel()
     
-    lazy var loadingView: UIActivityIndicatorView = {
-        let loadingView = UIActivityIndicatorView(frame: CGRect(x: 0, y: 100, width: 50, height: 50))
-        loadingView.color = .gray
-        loadingView.style = .medium
-        loadingView.startAnimating()
-        loadingView.center.x = self.view.center.x
-        return loadingView
-    }()
+    var checkView: UIView = UIView()
     
     lazy var seccessView: UIView = {
         var width = screenSize.width * 0.8
@@ -34,7 +28,7 @@ class ActivityApplyViewController: UIViewController {
         x = screenSize.width * 0.3
         y = screenSize.height * 0.1
         let imageView = UIImageView(frame: CGRect(x: x, y: y, width: width, height: heigth))
-        let image = UIImage(named: "cross")!.imageWithColor(newColor: .blue)
+        let image = UIImage(named: "check")!.imageWithColor(newColor: .blue)
         imageView.image = image
         view.addSubview(imageView)
         
@@ -43,7 +37,7 @@ class ActivityApplyViewController: UIViewController {
         x = 0
         y = screenSize.height * 0.22
         let textView = UITextView(frame: CGRect(x: x, y: y, width: width, height: heigth))
-        textView.text = "失敗"
+        textView.text = "成功"
         textView.font =  UIFont.boldSystemFont(ofSize: 48)
         textView.textColor = .blue
         textView.textAlignment = .center
@@ -83,15 +77,82 @@ class ActivityApplyViewController: UIViewController {
         return view
     }
     
+    lazy var acceptButton: LoaderButton = {
+        let width = screenSize.width * 0.3
+        let heigth = screenSize.width * 0.2
+        let x = screenSize.width * 0.5
+        let y = screenSize.height * 0.3
+        let checkButton = LoaderButton(frame: CGRect(x: x, y: y, width: width, height: heigth))
+        checkButton.setTitle("確定", for: .normal)
+        checkButton.layer.cornerRadius = 15
+        checkButton.backgroundColor = UIColor(rgb: 0x3638E0)
+        checkButton.addTarget(self, action: #selector(accept), for: .touchUpInside)
+        return checkButton
+    }()
+    
+    lazy var cancleButton: LoaderButton = {
+        let width = screenSize.width * 0.3
+        let heigth = screenSize.width * 0.2
+        let x = screenSize.width * 0
+        let y = screenSize.height * 0.3
+        let cancleButton = LoaderButton(frame: CGRect(x: x, y: y, width: width, height: heigth))
+        cancleButton.setTitle("取消", for: .normal)
+        cancleButton.layer.cornerRadius = 15
+        cancleButton.backgroundColor = UIColor(rgb: 0xE0385F)
+        cancleButton.addTarget(self, action: #selector(cancle), for: .touchUpInside)
+        return cancleButton
+    }()
+    
+    func confirmView(activity: Activity) -> UIView {
+        var width = screenSize.width * 0.8
+        var heigth = self.view.frame.height
+        var x = screenSize.width * 0.1
+        var y = 0.0
+        let view = UIView(frame: CGRect(x: x, y: y, width: width, height: heigth))
+        
+        width = screenSize.width * 0.8
+        heigth = 128
+        x = 0
+        y = screenSize.height * 0.05
+        let textView = UILabel(frame: CGRect(x: x, y: y, width: width, height: heigth))
+        textView.text = "確定要報名以下活動?\n\"\(activity.name)\""
+        textView.numberOfLines = 4
+        textView.font =  UIFont.systemFont(ofSize: 20)
+        textView.textAlignment = .center
+        view.addSubview(textView)
+        
+        view.addSubview(acceptButton)
+        view.addSubview(cancleButton)
+        
+        return view
+    }
+    
+    @objc func accept() {
+        acceptButton.isLoading = true
+        cancleButton.isEnabled = false
+        Task {
+            let result = await activityApplyViewModel.applyActivity(activity:activity)
+            self.checkView.removeFromSuperview()
+            if result == "成功" {
+                self.view.addSubview(seccessView)
+            } else {
+                self.view.addSubview(failView(message: result))
+            }
+        }
+    }
+    
+    @objc func cancle() {
+        dismiss(animated: true)
+    }
+    
+    
     // MARK: viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
         
-        view.addSubview(loadingView)
-        
-        loadingView.stopAnimating()
-//        view.addSubview(seccessView)
-        view.addSubview(failView(message: "這功能還沒做"))
+        checkView = confirmView(activity: activity)
+        view.addSubview(checkView)
+//        view.addSubview(failView(message: "這功能還沒做"))
     }
 }
--- NIU App/NIU App/Activity/Apply/ActivityApplyViewModel.swift
+++ NIU App/NIU App/Activity/Apply/ActivityApplyViewModel.swift
@@ -6,7 +6,79 @@
 //
 
 import Foundation
+import Kanna
 
 class ActivityApplyViewModel {
     
+    let password: String
+    let account: String
+    
+    func applyActivity(activity: Activity) async -> String {
+        let loginURL = URL(string: "https://ccsys.niu.edu.tw/MvcTeam/Account/Login")!
+        var getLoginPageRequest = URLRequest(url: loginURL)
+        getLoginPageRequest.allHTTPHeaderFields = headers
+            
+        do {
+            let (loginData, _) = try await URLSession.shared.data(for: getLoginPageRequest)
+            let loginPageHTML = String(decoding: loginData, as: UTF8.self)
+            var doc = try? HTML(html: loginPageHTML, encoding: .utf8)
+            // 設定Payload
+            var __RequestVerificationToken = doc!.at_css("input")!["value"]
+            var urlParser = URLComponents()
+            urlParser.queryItems = [
+                URLQueryItem(name: "__RequestVerificationToken", value: __RequestVerificationToken),
+                URLQueryItem(name: "Account", value: account),
+                URLQueryItem(name: "Password", value: password),
+            ]
+            var httpBodyString = urlParser.percentEncodedQuery!.urlEncoded()
+            // 傳送登入資訊
+            var loginedRequest = URLRequest(url: loginURL)
+            loginedRequest.httpMethod = "POST"
+            loginedRequest.httpBody = httpBodyString.data(using: .ascii)
+            loginedRequest.allHTTPHeaderFields = headers
+            var (loginedData, _) = try await URLSession.shared.data(for: loginedRequest)
+            var loginedHTML = String(decoding: loginedData, as: UTF8.self)
+            if (!loginedHTML.contains("您好")) {
+                return "伺服器錯誤"
+            }
+            let activityURL = URL(string: "https://ccsys.niu.edu.tw/MvcTeam/Act/Apply/\(activity.id)")!
+            var activityRequest = URLRequest(url: activityURL)
+            activityRequest.allHTTPHeaderFields = headers
+            let (activityData, _) = try await URLSession.shared.data(for: activityRequest)
+            let activityHTML = String(decoding: activityData, as: UTF8.self)
+            doc = try? HTML(html: activityHTML, encoding: .utf8)
+            // 設定Payload
+            __RequestVerificationToken = doc!.at_css("input")!["value"]
+            urlParser = URLComponents()
+            urlParser.queryItems = [
+                URLQueryItem(name: "__RequestVerificationToken", value: __RequestVerificationToken),
+                URLQueryItem(name: "Account", value: account),
+                URLQueryItem(name: "Password", value: password),
+            ]
+            httpBodyString = urlParser.percentEncodedQuery!.urlEncoded()
+            // 傳送登入資訊
+            var applyedRequest = URLRequest(url: activityURL)
+            applyedRequest.httpMethod = "POST"
+            applyedRequest.httpBody = httpBodyString.data(using: .ascii)
+            applyedRequest.allHTTPHeaderFields = headers
+            let (applyedData, _) = try await URLSession.shared.data(for: applyedRequest)
+            // 檢查報名狀態
+            let applyedHTML = String(decoding: applyedData, as: UTF8.self)
+            if (!applyedHTML.contains("已報名")) {
+                return "伺服器錯誤"
+            }
+            print("SUCCESS")
+        }catch {
+            print("ERROR")
+            return "伺服器錯誤"
+        }
+        return "成功"
+    }
+    
+    // MARK: init
+    init () {
+        let userDefault = UserDefaults()
+        account = userDefault.value(forKey: "account") as! String
+        password = userDefault.value(forKey: "password") as! String
+    }
 }
